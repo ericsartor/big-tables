928d75ef4fe0144e6c98fd80ca01fe79bc54fe3a
Eric <ericdavidsartor@live.ca>
2018-06-23 00:47:51 -0400

    created separation between private and public members in BigTable class

--- src/BigTable.js
+++ src/BigTable.js
@@ -1,57 +1,11 @@
-class BigTable {
-  constructor(itemList, options) {
-    /* set properties bsaed on user input */
+function BigTable(itemList, options) {
+  /* PRIVATE METHODS */
 
-    this.options = options;
-    this.objects = itemList;
-    this.containerClass = options.containerClass || null;
-    this.columnClass = options.columnClass || null;
-    this.cellClass = options.cellClass || null;
-
-    this.propertyMode = options.propertyMode || 'mutual';
-
-    // create the property list depending on the property mode
-    if (!Array.isArray(this.propertyMode)) {
-      switch (this.propertyMode) {
-        case 'all':
-          this.properties = this.findAllProperties(this.objects);
-          break;
-        case 'mutual':
-          this.properties = this.findMutualProperties(this.objects);
-          break;
-      }
-    } else {
-      this.properties = this.propertyMode;
-    }
-
-    // set column width value
-    if (options.columnWidths) {
-      this.gridTemplate = options.columnWidths.map((widthValue) => {
-        return widthValue + 'fr';
-      });
-    } else {
-      // column widths default to 1fr
-      this.gridTemplate = '1fr,'.repeat(this.properties.length).split(',');
-      this.gridTemplate.pop(); // last element in array will be empty
-    }
-
-    /* initialize some internal properties */
-
-    this.node = this.createContainer();
-    this.offset = 0;
-    this.rowCount = 20;
-
-    // create scroll bar if required
-    if (options.scrollBar) {
-      this.createScrollBar();
-    }
-  }
-
-  createContainer() {
+  const createContainer = () => {
     const container = document.createElement('div');
-    container.className = `big-table-container ${this.containerClass || ''}`;
+    container.className = `big-table-container ${this._props.containerClass || ''}`;
     container.style.display = 'grid';
-    container.style.gridTemplate = `1fr / ${this.gridTemplate.join(' ')}${this.options.scrollBar ? ` 30px` : ''}`;
+    container.style.gridTemplate = `1fr / ${this._props.gridTemplate.join(' ')}${this._props.options.scrollBar ? ` 30px` : ''}`;
 
     container.addEventListener('wheel', (e) => {
       // determine determine how many steps to count the scroll as
@@ -71,24 +25,24 @@ class BigTable {
         }
       }
 
-      this.performScroll(scrollCount);
+      performScroll(scrollCount);
     });
 
     return container;
   }
 
-  createColumn() {
+  const createColumn = () => {
     const columnDiv = document.createElement('div');
-    columnDiv.className = `big-table-column ${this.columnClass || ''}`;
+    columnDiv.className = `big-table-column ${this._props.columnClass || ''}`;
     
-    const gridTemplateArr = '1fr,'.repeat(this.rowCount).split(',');
+    const gridTemplateArr = '1fr,'.repeat(this._props.rowCount).split(',');
     gridTemplateArr.pop(); // last element in array will be empty
     columnDiv.style.gridTemplate = `${gridTemplateArr.join(' ')} / 1fr`;
 
     return columnDiv;
   }
 
-  createHeader(headerName) {
+  const createHeader = (headerName) => {
     const headerDiv = document.createElement('div');
     headerDiv.className = 'big-table-header';
     headerDiv.textContent = headerName;
@@ -97,10 +51,10 @@ class BigTable {
     return headerDiv;
   }
 
-  createValueCell(value, rowNumber, columnName) {
+  const createValueCell = (value, rowNumber, columnName) => {
     const valueCellDiv = document.createElement('div');
     valueCellDiv.className = `big-table-value-cell big-table-row-${rowNumber}` +
-      ` big-table-${columnName}-value-cell ${this.cellClass || ''}`;
+      ` big-table-${columnName}-value-cell ${this._props.cellClass || ''}`;
     valueCellDiv.textContent = value;
 
     const tableContainer = this.node;
@@ -124,22 +78,22 @@ class BigTable {
     return valueCellDiv;
   }
 
-  draw() {
+  const draw = () => {
     // create document fragments for the value cells for each column div
     // simultaneously create the header divs for each column
     const columnFragments = {};
-    for (const headerTitle of this.properties) {
+    for (const headerTitle of this.columnHeaders) {
       columnFragments[headerTitle] = document.createDocumentFragment();
 
-      const headerDiv = this.createHeader(headerTitle);
+      const headerDiv = createHeader(headerTitle);
       columnFragments[headerTitle].appendChild(headerDiv);
 
       // create all the value cells for this column
-      for (let i = this.offset; i < this.rowCount + this.offset; i++) {
+      for (let i = this.offset; i < this._props.rowCount + this.offset; i++) {
         const currentObj = this.objects[i];
         const cellValue = currentObj[headerTitle];
         
-        const valueCellDiv = this.createValueCell(cellValue, i, headerTitle);
+        const valueCellDiv = createValueCell(cellValue, i, headerTitle);
         columnFragments[headerTitle].appendChild(valueCellDiv);
       }
     }
@@ -147,13 +101,13 @@ class BigTable {
     // create the columns
     const columnDivs = document.createDocumentFragment();
     for (const headerTitle in columnFragments) {
-      const columnDiv = this.createColumn();
+      const columnDiv = createColumn();
       columnDiv.appendChild(columnFragments[headerTitle]);
       columnDivs.appendChild(columnDiv);
     }
     
     // erase the whole table
-    if (this.options.scrollBar) {
+    if (this._props.options.scrollBar) {
       for (let i = this.node.children.length - 1; i >= 0; i--) {
         if (!this.node.children[i].classList.contains('big-table-scroll-bar-container')) {
           this.node.children[i].remove();
@@ -169,9 +123,9 @@ class BigTable {
     this.node.appendChild(columnDivs);
   }
 
-  hideColumn(columnNameOrIndex) {
+  const hideColumn = (columnNameOrIndex) => {
     const columnName = Utils.isString(columnNameOrIndex) ? columnNameOrIndex
-      : this.properties[Number(columnNameOrIndex)];
+      : this.columnHeaders[Number(columnNameOrIndex)];
 
     const columnValueCells = this.node.querySelectorAll(`${columnName}-value-cell`);
 
@@ -183,20 +137,20 @@ class BigTable {
   /* SCROLLING STUFF */
 
   // returns the maximum the table offset can be without showing blank space
-  determineMaxOffset() {
-    return this.objects.length - this.rowCount;
+  const determineMaxOffset = () => {
+    return this.objects.length - this._props.rowCount;
   }
 
-  determineMaxScrollBarTop() {
-    return 100 - parseFloat(this.scrollBarHead.style.height);;
+  const determineMaxScrollBarTop = () => {
+    return 100 - parseFloat(this._props.scrollBarHead.style.height);;
   }
 
-  determineScrollBarScalingAmount() {
+  const determineScrollBarScalingAmount = () => {
     let amount = 100;
 
-    if (this.objects.length <= this.rowCount) return amount;
+    if (this.objects.length <= this._props.rowCount) return amount;
 
-    const excessRows = this.objects.length - this.rowCount;
+    const excessRows = this.objects.length - this._props.rowCount;
 
     let subtractAmount = 1;
     for (let i = 0; i < excessRows; i++) {
@@ -210,65 +164,65 @@ class BigTable {
     return amount;
   }
 
-  createScrollBar() {
+  const createScrollBar = () => {
     const scrollBarContainer = document.createElement('div');
     scrollBarContainer.className = 'big-table-scroll-bar-container';
-    this.scrollBarContainer = scrollBarContainer;
+    this._props.scrollBarContainer = scrollBarContainer;
 
     const scrollBarHead = document.createElement('div');
     scrollBarHead.className = 'big-table-scroll-bar-head';
-    scrollBarHead.style.height = this.determineScrollBarScalingAmount() + '%';
-    this.scrollBarHead = scrollBarHead;
+    scrollBarHead.style.height = determineScrollBarScalingAmount() + '%';
+    this._props.scrollBarHead = scrollBarHead;
 
     const that = this;
     scrollBarHead.addEventListener('mousedown', (e) => {
-      that.grabbingScrollBar = true;
-      that.scrollBarGrabPreviousY = e.clientY;
+      that._props.grabbingScrollBar = true;
+      that._props.scrollBarGrabPreviousY = e.clientY;
     });
 
     window.addEventListener('mouseup', () => {
-      that.grabbingScrollBar = false;
-      that.scrollBarGrabPreviousY = null;
+      that._props.grabbingScrollBar = false;
+      that._props.scrollBarGrabPreviousY = null;
     });
 
     window.addEventListener('mousemove', (e) => {
-      if (!that.grabbingScrollBar) return;
+      if (!that._props.grabbingScrollBar) return;
 
-      const containerRect = this.scrollBarContainer.getBoundingClientRect();
+      const containerRect = this._props.scrollBarContainer.getBoundingClientRect();
 
       if (e.clientY < containerRect.top) {
-        this.performScroll(-this.offset);
+        performScroll(-this.offset);
       } else if (e.clientY > containerRect.bottom) {
-        this.performScroll(this.determineMaxOffset() - this.offset);
+        performScroll(determineMaxOffset() - this.offset);
       } else {
-        const totalPixelsOfMovement = this.determineMaxScrollBarTop() / 100 * containerRect.height;
+        const totalPixelsOfMovement = determineMaxScrollBarTop() / 100 * containerRect.height;
         const rowsPerPixel = this.objects.length / totalPixelsOfMovement;
-        const moveAmountPixels = e.clientY - that.scrollBarGrabPreviousY;
+        const moveAmountPixels = e.clientY - that._props.scrollBarGrabPreviousY;
         const totalRowsMoved = rowsPerPixel * moveAmountPixels;
   
         
-        this.performScroll(Math.round(totalRowsMoved));
+        performScroll(Math.round(totalRowsMoved));
       }
 
-      that.scrollBarGrabPreviousY = e.clientY;
-      this.updateScrollHead();
+      that._props.scrollBarGrabPreviousY = e.clientY;
+      updateScrollHead();
     })
 
     scrollBarContainer.appendChild(scrollBarHead);
     this.node.appendChild(scrollBarContainer);
   }
 
-  updateScrollHead() {
-    const maximumTop = this.determineMaxScrollBarTop();
-    const maxOffset = this.determineMaxOffset();
+  const updateScrollHead = () => {
+    const maximumTop = determineMaxScrollBarTop();
+    const maxOffset = determineMaxOffset();
     const newTop = (this.offset / maxOffset) * maximumTop;
-    this.scrollBarHead.style.top = newTop + '%';
+    this._props.scrollBarHead.style.top = newTop + '%';
   }
 
   // updates the table offset then re-draws table
-  performScroll(steps) {
+  const performScroll = (steps) => {
     const desiredOffset = this.offset + steps;
-    const notTooHigh = desiredOffset <= this.determineMaxOffset();
+    const notTooHigh = desiredOffset <= determineMaxOffset();
     
     if (notTooHigh && desiredOffset >= 0) {
       this.offset = desiredOffset;
@@ -276,14 +230,14 @@ class BigTable {
       return;
     }
 
-    this.updateScrollHead();
-    this.draw();
+    updateScrollHead();
+    draw();
   }
 
   /* UTILITIES */
 
   // loop through an array of objects and find the properties common between them
-  findMutualProperties(arrOfObjs) {
+  const findMutualProperties = (arrOfObjs) => {
     // build an initial list of properties to whittle down
     let mutualProperties = [];
     for (const prop in arrOfObjs[0]) {
@@ -306,7 +260,7 @@ class BigTable {
     return mutualProperties;
   }
 
-  findAllProperties(arrOfObjs) {
+  const findAllProperties = (arrOfObjs) => {
     const allProperties = [];
     arrOfObjs.forEach((obj) => {
       for (const prop in obj) {
@@ -318,4 +272,108 @@ class BigTable {
 
     return allProperties;
   }
-};
+
+  /* PUBLIC METHODS AND PROPERTIES */
+
+  this.place = (options) => {
+    if (!options) {
+      throw Utils.generateError(`Tried to append table to page but no options` +
+      ` object was supplied.`);
+    }
+
+    if (options.appendChild) {
+      let parentNode;
+
+      if (Utils.isString(options.appendChild)) {
+        parentNode = document.getElementsById(options.appendChild);
+
+        if (parentNode === null) {
+          throw Utils.generateError(`Tried to append table to page using` +
+            ` appendChild option, but string argument was not an existing ID: ` +
+            options.appendChild);
+        }
+      } else if (options.appendChild instanceof HTMLElement) {
+        parentNode = options.appendChild;
+      } else {
+        throw Utils.generateError(`Tried to append table to page using appendChild` +
+          ` option, but argument was neither a string ID or an HTMLElement: ` +
+          options.appendChild);
+      }
+      
+      parentNode.appendChild(this.node);
+    } else if (options.insertBefore) {
+      let insertBeforeNode;
+
+      if (Utils.isString(options.insertBefore)) {
+        insertBeforeNode = document.getElementsById(options.insertBefore);
+
+        if (insertBeforeNode === null) {
+          throw Utils.generateError(`Tried to append table to page using insertBefore` +
+            ` option, but string argument was not an existing ID: ` +
+            options.insertBefore);
+        }
+      } else if (options.appendChild instanceof HTMLElement) {
+        insertBeforeNode = options.insertBefore;
+      } else {
+        throw Utils.generateError(`Tried to append table to page using insertBefore` +
+          ` option, but argument was neither a string ID or an HTMLElement: ` +
+          options.insertBefore);
+      }
+      
+      insertBeforeNode.parentNode.insertBefore(this.node, insertBeforeNode);
+    } else {
+      throw Utils.generateError(`Tried to append table but did not supply any of the` +
+        ` valid options (appendChild, insertBefore) with either a string ID or` +
+        ` HTMLElement node.`);
+    }
+
+    draw();
+  }
+
+  /* CONSTRUCTOR *//* CONSTRUCTOR *//* CONSTRUCTOR *//* CONSTRUCTOR */
+
+  this._props = {}; // holds all the private properties that the user won't need to see
+  this._props.options = options;
+  this.objects = itemList;
+  this._props.containerClass = options.containerClass || null;
+  this._props.columnClass = options.columnClass || null;
+  this._props.cellClass = options.cellClass || null;
+
+  this._props.propertyMode = options.propertyMode || 'mutual';
+
+  // create the property list depending on the property mode
+  if (!Array.isArray(this._props.propertyMode)) {
+    switch (this._props.propertyMode) {
+      case 'all':
+        this.columnHeaders = findAllProperties(this.objects);
+        break;
+      case 'mutual':
+        this.columnHeaders = findMutualProperties(this.objects);
+        break;
+    }
+  } else {
+    this.columnHeaders = this._props.propertyMode;
+  }
+
+  // set column width value
+  if (options.columnWidths) {
+    this._props.gridTemplate = options.columnWidths.map((widthValue) => {
+      return widthValue + 'fr';
+    });
+  } else {
+    // column widths default to 1fr
+    this._props.gridTemplate = '1fr,'.repeat(this.columnHeaders.length).split(',');
+    this._props.gridTemplate.pop(); // last element in array will be empty
+  }
+
+  /* initialize some internal properties */
+
+  this.node = createContainer();
+  this.offset = 0;
+  this._props.rowCount = 20;
+
+  // create scroll bar if required
+  if (options.scrollBar) {
+    createScrollBar();
+  }
+}
